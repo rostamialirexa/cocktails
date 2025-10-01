e0a4f30f4a7a12e6b497d7c2e6c82811de74faee
Alirexa <rostamialireza42@gmail.com>
2025-10-01 11:36:54 +0330

    Implement contact section -2

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { allCocktails } from '../../constants'
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
