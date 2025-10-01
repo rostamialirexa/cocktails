b2212e25e17943081ae170863c22dbf19b516048
Alirexa <rostamialireza42@gmail.com>
2025-10-01 11:35:45 +0330

    Implement contact section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
 import About from "./components/About"
 import Art from "./components/Art"
+import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -15,7 +17,9 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
-            <Art/>
+            <Art />
+            <Menu />
+            <Contact />
         </main>
     )
 }
